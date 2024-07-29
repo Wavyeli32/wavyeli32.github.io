30c209b23382f5999b3002e04a382c774991d6ee
Elijah Everett <145284361+Wavyeli32@users.noreply.github.com>
2024-07-29 03:38:15 -0400

    Update 2024-7-29-Hello-World.m

--- _posts/2024-7-29-Hello-World.m
+++ _posts/2024-7-29-Hello-World.m
@@ -1,1 +1,8 @@
+---
+layout: post
+title: Github Labs
+---
+
+Most of my time I spent navigating and going through all of the labs.I took my time in the labs making sure I was reading the instructions and completing the exercises. I learned the importance of helping them create labs for other people to learn. They will provide you with a template to follow. They have different sections such as the items marked “planned” with “-1” are those we intend to do first; “planned” with “-2” are planned in a second pass, “planned” with “-0” were done early. The term “planned” is replaced with “done” as they’re done. The ones marked “unassigned” are ones where no one has agreed to work on yet. Some next steps I can take is to work on the unassigned lab. My internal thought process for what I want to do next is the labs that I can understand and be able to complete.
+
 
